ad5098a034770a7966f80f540bb41673ca037e69
Aian <167414305+dffses@users.noreply.github.com>
2026-09-23 14:11:40 +0200

    fork01.c completado

--- UT1/fork01.c
+++ UT1/fork01.c
@@ -1,1 +1,40 @@
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+void main(){
+    pid_t pid,pid_hijo, pid_padre;
+
+    pid=fork();
+
+    if (pid==-1)
+    {
+        printf("Error");
+        exit(-1);
+    }
+    if (pid==0)
+    {
+        pid_hijo=getpid();
+        pid_padre=getppid();
+        printf("Soy el proceso HIJO\n");
+        printf("mi PID es: %d\n",pid_hijo);
+        printf("El PID de mi padre es: %d\n",pid_padre);
+
+    }else{
+        pid_hijo=wait(NULL);
+        pid_padre=getpid();
+        
+        printf("Soy el proceso PADRE\n");
+        printf("mi PID es: %d\n",pid_padre);
+        printf("Mi HIJO tiene el PID: %d\n",pid_hijo);
+        printf("El proceso HIJO ha terminado\n");
+
+    }
+    
+    exit(0);
+
+}
