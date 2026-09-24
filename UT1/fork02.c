ad579a0b181471756218aa776be4d523767b60aa
Aian <167414305+dffses@users.noreply.github.com>
2026-09-24 13:54:04 +0200

    corregido fork02.c
    Updated child process PID retrieval and print statements.

--- UT1/fork02.c
+++ UT1/fork02.c
@@ -7,7 +7,7 @@
 
 void main(){
     pid_t pid, pid_hijo, pid_padre;
-    pid_hijo=getpid();
+
     pid =fork();
 
       if (pid==-1)
@@ -21,13 +21,12 @@ void main(){
         pid_hijo=getpid();
         pid_padre=getppid();
         printf ("Soy el hijo y mi nombre es IAN NARANJO CAIZA\n");
-        sleep(5);
 
     }else{
         wait(NULL);
         pid_padre=getpid();
 
-        printf("MI hijo tiene el pid: %d\n",pid_hijo);
+        printf("MI hijo tiene el pid: %d\n",pid);
         printf("Soy el padre y tengo el pid: %d\n",pid_padre);
 
 
