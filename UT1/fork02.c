6939c097527b96507b3a94bf9700a46e6e803a3c
Aian <167414305+dffses@users.noreply.github.com>
2026-09-24 12:09:14 +0200

    editando fork02.c
    Added a fork example demonstrating parent and child process behavior.

--- UT1/fork02.c
+++ UT1/fork02.c
@@ -1,1 +1,35 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+
+void main(){
+    pid_t pid, pid_hijo, pid_padre;
+
+    pid =fork();
+
+      if (pid==-1)
+    {
+        printf("Error");
+        exit(-1);
+    }
+    if (pid==0)
+    {
+
+        pid_hijo=getpid();
+        pid_padre=getppid();
+        printf ("Soy el hijo y mi nombre es IAN NARANJO CAIZA\n");
+
+    }else{
+        pid_hijo=wait(NULL);
+        pid_padre=getpid();
+
+        printf("MI hijo tiene el pid%d\n",pid_hijo);
+        printf("Soy el padre y tengo el pid: %d\n",pid_padre);
+
+
+    }
+    exit(0);
+}
